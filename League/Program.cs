df16b5ae16c55aec605ab1e592dda067da6ba268
Shawn Dempsey <shwanton@gmail.com>
2018-10-09 13:40:51 -0700

    Add error handling for reading file

--- League/Program.cs
+++ League/Program.cs
@@ -7,11 +7,21 @@ namespace League
     {
         static void Main(string[] args)
         {
-            var file = File.ReadAllText(args[0]);
-            var data = file.Split("\n");
-            foreach (var line in data)
+            try
             {
-                Console.WriteLine(line);
+                if (args == null && args.Length == 0)
+                    throw new FileNotFoundException();
+
+                var file = File.ReadAllText(args[0]);
+                var data = file.Split("\n");
+                foreach (var line in data)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("There was a problem loading the file");
             }
         }
     }
