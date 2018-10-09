1c07d6c2fe5333e41684c9ff2317b64058334c60
Shawn Dempsey <shwanton@gmail.com>
2018-10-09 13:27:27 -0700

    Initial commit

---
+++ League/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace League
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
