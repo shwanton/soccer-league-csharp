6f3dfe2db9c23b78e2dbf6f0dabd362fe4f94105
Shawn Dempsey <shwanton@gmail.com>
2018-10-10 14:52:38 -0700

    Add integration test

---
+++ Tests/IntegrationTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Abstractions.TestingHelpers;
+using League;
+using Xunit;
+
+namespace Tests
+{
+    public class IntegrationTests
+    {
+        [Fact]
+        public void Integration_ShouldPrintLeagueStats()
+        {
+            var fileName = "test.txt";
+            var input = Fixtures.Input();
+
+            var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>
+            {
+                {
+                    fileName, new MockFileData(input)
+                }
+            });
+            var loader = new FileLoader(fileSystem, fileName);
+            var data = loader.LoadData();
+
+            var parsed = new GameParser(data).Parse();
+            var stats = new StatsCalulator(parsed).Calculate();
+            var sorted = new Sorter(stats).Sort();
+            var ranked = new Ranker(sorted).Rank();
+            var result = new Printer(ranked).Print();
+
+            var expected = Fixtures.Output();
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
